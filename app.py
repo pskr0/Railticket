f15ecc97be245471050846a05d4c1ac11d3bc1a7
pskr0 <47482674+pskr0@users.noreply.github.com>
2020-06-24 16:19:20 +0530

    UPDATED With Addi. features

--- app.py
+++ app.py
@@ -11,34 +11,45 @@ def home():
 
 @app.route('/predict',methods=['POST','GET'])
 def predicts():
-    projectpath = request.form['inputdata']
+    #projectpath = request.form['inputdata']
     #int_features = [str(x) for x in request.form.values()]
     #projectpath = request.form('miniresult')
     #final_features = [np.array(int_features)]
+    source00 = request.form['sourcename']
+    destination00=request.form['destinationname']
+    projectpath = request.form['inputdata']
+
     a=int(str(projectpath))
     aa=a+1
     aaa=a+2
     aaaa=a+3
+    aaaaa=a+4
+
     projectpath1=str(int(aa))
+    projectpath2=str(int(aaa))
+    projectpath3=str(int(aaaa))
+    projectpath4=str(int(aaaaa))
 
     prediction = model.predict(projectpath)
     prediction1 = model.predict(projectpath1)
-    #prediction2 = model.predict(projectpath2)
-    #prediction3 = model.predict(projectpath3)
-    #prediction4 = model.predict(projectpath4)
+    prediction2 = model.predict(projectpath2)
+    prediction3 = model.predict(projectpath3)
+    prediction4 = model.predict(projectpath4)
     #prediction = model.predict(projectpath)
     #prediction = model.predict(projectpath)
     output = round(prediction[0]*10, 3)
-    output1 = round(prediction1[0], 3)
-    #output2 = round(prediction[0], 3)
-    #output3 = round(prediction[0], 3)
-    #output4 = round(prediction[0], 3)
+    output1 = round(prediction1[0]*10, 3)
+    output2 = round(prediction2[0]*10, 3)
+    output3 = round(prediction3[0]*10, 3)
+    output4 = round(prediction4[0]*10, 3)
     
 
     #output = round(prediction[0], 3)
     #output1=output
-    return render_template('index.html', prediction_text='Ticket Conformation Probability {}'.format(output))
-    render_template('index.html', pred1='Ticket Conformation  $ {}'.format(output1))
+    return render_template('index.html',Date0="DATE [YYYY/MM/DD]",head2="PROBABILITY OF COMFORMATION",prediction_text='{} %'.format(output),pred='{} %'.format(output1),source0='{} =========>>>>'.format(source00),destination0="==========>>>> {}".format(destination00),dat0="{} ".format(projectpath),dat1="{} ".format(projectpath1),dat2="{} ".format(projectpath2),dat3=" {}".format(projectpath3),dat4="{} ".format(projectpath4),pred1='{} %'.format(output2),pred2='{} %'.format(output3),pred3='{} %'.format(output4))
+
+
+
     #return render_template('index.html', pred1='Ticket Conformation Should be  $ {}'.format(output2))
     #return render_template('index.html', pred2='Ticket Conformation Should be  $ {}'.format(output3))
     #return render_template('index.html', pred3='Ticket Conformation Should be  $ {}'.format(output4))
